4b1c936025432ae6696837eb40a237ed3e1f6c6e
Robin <rb.wesselmann@gmail.com>
2024-11-12 01:28:41 +0100

    add cleaning steps + fill database with relevant data

--- 03_Project_Disaster_Response_Pipeline/data/process_data.py
+++ 03_Project_Disaster_Response_Pipeline/data/process_data.py
@@ -1,16 +1,55 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Load the messages and categories under the given filepath,
+    concatenate messages and categories and output the result.
 
+    Args:
+        messages_filepath (str): filepath for the message dataset (csv)
+        categories_filepath (str): filepath for the category dataset (csv)
+    """
+    df_msg = pd.read_csv(messages_filepath)
+    df_cat = pd.read_csv(categories_filepath)
+    df_ = pd.concat([df_msg, df_cat.drop("id", axis = 1)], axis = 1)
+    return df_
 
 def clean_data(df):
-    pass
+    """Splits the categories column into separate, clearly named columns
+      converts values to binary, and drops duplicates.
+
+    Args:
+        df (pd.DataFrame): messages and categories data
+    """
+    
+    #expand the categories column 
+    df_categories = df["categories"].str.split(";", expand = True)
+    col_names = [col.split("-")[0] for col in df_categories.iloc[0]]
+    df_categories.columns = col_names
+
+    for col in df_categories:
+        df_categories[col] = df_categories[col].str.split("-").str[1]
+
+    #transform to binary 
+    df_categories = df_categories.astype("int")
+    
+    #drop categories column from df
+    df = df.drop("categories", axis = 1)
+
+    #combine expanded df_categories & msg data (df)
+    df_expanded = pd.concat([df, df_categories], axis = 1)
+
+    #drop duplicates
+    df_clean= df_expanded.drop_duplicates()
+
+    return df_clean
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}') #'sqlite:///InsertDatabaseName.db'
+    df.to_sql('disaster_reponse_tbl', engine, index=False)  
 
 
 def main():
