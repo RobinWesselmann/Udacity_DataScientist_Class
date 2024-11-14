51f3e91639d490b40a498a6566ebaa60a9f6f4c4
Robin <rb.wesselmann@gmail.com>
2024-11-14 22:53:01 +0100

    change train_classifier.py

--- 03_Project_Disaster_Response_Pipeline/models/train_classifier.py
+++ 03_Project_Disaster_Response_Pipeline/models/train_classifier.py
@@ -1,30 +1,101 @@
+# import libraries
 import sys
+import pandas as pd 
+import numpy as np
+from sqlalchemy import create_engine 
 
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+import re
+from utils import tokenize
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+
+nltk.download(['punkt', 'punkt_tab', 'wordnet', 'averaged_perceptron_tagger'])
+
+import warnings
+import pickle
+
+warnings.filterwarnings("ignore")
 
 def load_data(database_filepath):
-    pass
+    """Load the relevant data from the database in the given filepath
 
+    Args:
+        database_filepath (str): database filepath
+    """
+    #read data from db
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql("SELECT * FROM disaster_reponse_tbl", con = engine)
 
-def tokenize(text):
-    pass
+    #initially clean data concerning basic topics
+    df.drop("child_alone", axis = 1, inplace=True)
+    df["related"] = df["related"].map(lambda x: 1 if x==2 else x)
+    df["direct_report"] = df["direct_report"].map(lambda x: 1 if x==2 else x)
 
+    X = df["message"]
+    y = df[df.columns[4:]].astype("int")
 
-def build_model():
-    pass
+    return X, y, y.columns.tolist()
 
+def build_model(clf = RandomForestClassifier()):
+    """Build a pipeline containing CountVectorizer incl. tokenizer, TfidfTransformer, classifier
+
+    Args:
+        clf (model): Prediction model
+
+    Returns:
+        pipeline: sklearn.pipeline.Pipeline
+    """
+
+    pipeline = Pipeline([
+        ("features", FeatureUnion([
+            ("text_pipeline", Pipeline([
+                ("vect", CountVectorizer(tokenizer=tokenize)),
+                ("tfidf", TfidfTransformer())
+            ]))
+        ],n_jobs=-1)),
+        ("clf", MultiOutputClassifier(clf,n_jobs=-1))
+    ])
+
+    return pipeline
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """Evaluate the model and print out the sklearn classification_report
 
+    Args:
+        model (variable): Prediction model to evaluate
+        X_test (pandas.Series):  Messages
+        Y_test (pandas.DataFrame): Categories
+        category_names (list): Category names
+    """
+    
+    y_pred_test = model.predict(X_test)
+    print(classification_report(Y_test.values, y_pred_test, target_names=category_names))
 
 def save_model(model, model_filepath):
-    pass
+    """Save the trained model
 
+    Args:
+        model (variable): Prediction model to evaluate
+        model_filepath (str): path to save the model
+    """
+    
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
---
+++ 03_Project_Disaster_Response_Pipeline/models/utils.py
@@ -0,0 +1,37 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+import re
+
+nltk.download(['punkt', 'punkt_tab', 'wordnet', 'averaged_perceptron_tagger'])
+
+def tokenize(text):
+    """Principal cleaning steps (substitute urls, normalization, delete unnecessary spaces),
+    tokenization & lemmatization for a given text
+
+    Args:
+        text (str): text to preprocess 
+
+    Returns:
+        clean_tokens(list): clean word tokens for further preprocessing 
+    """
+
+    #replace all urls
+    url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    #create tokens & lemmatize
+    tokens = word_tokenize(text)
+    
+    #lemmatize, Normalize
+    lemmatizer = WordNetLemmatizer()
+        
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
